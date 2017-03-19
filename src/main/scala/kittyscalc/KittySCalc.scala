6cedac2d8f56cfc528d9602bbf14bf6e049ae55e
David Nemeth Csoka <encse@csokavar.hu>
2017-03-19 21:18:33 +0100

    BBEEEUUUUUUAAAAAHHHHH...
                        %%%%%%
                       %%%% = =
                       %%C    >
                        _)' _( .' ,
                     __/ |_/\   " *. o
                    /` \_\ \/     %`= '_  .
                   /  )   \/|      .^',*. ,
                  /' /-   o/       - " % '_
                 /\_/     <       = , ^ ~ .
                 )_o|----'|          .`  '
             ___// (_  - (\
            ///-(    \'   \\ b'ger

--- src/main/scala/kittyscalc/KittySCalc.scala
+++ src/main/scala/kittyscalc/KittySCalc.scala
@@ -1,4 +1,4 @@
-package kittyscalc
+//package kittyscalc
 
 import java.io.{File, FileInputStream, FileOutputStream, PrintStream}
 
@@ -12,12 +12,12 @@ class Data(var u:Long, var uWithDist:Long, var res:Long=0, var version:Int){
 
 class Node(val inode:Int, var nodeParent:Node = null) {
   var data:Data = null
-  var edges = mutable.Set[Edge]()
+  var edges = mutable.ListBuffer[Edge]()
   var notSeenEdges = mutable.Set[Edge]()
 
   def addEdge(edge: Edge) = {
     notSeenEdges.add(edge)
-    edges.add(edge)
+    edges.append(edge)
   }
 
   private def otherNode(nodes: IndexedSeq[Node], node: Node, edge: Edge): Node = {
@@ -88,28 +88,17 @@ object Solution {
     var ksSorted = ks.toList.sortBy(node => -node.getDistanceFromRoot())
     var restNodes = mutable.Set[Node]()
 
-    while (ksSorted.length + restNodes.size >= 1) {
-
-      var ksCurrent: Iterable[Node] = Nil
-      if (ksSorted.nonEmpty && (restNodes.isEmpty || ksSorted.head.getDistanceFromRoot() == restNodes.head.getDistanceFromRoot())) {
-        val d = ksSorted.head.getDistanceFromRoot()
-        val (ksCurrentT, ksSortedT) = ksSorted.span(node => node.getDistanceFromRoot() == d)
-        ksCurrent = ksCurrentT
-        ksSorted = ksSortedT
-      }
-
-      var restNodesNew = mutable.Set[Node]()
-      for {
-        set <- List(ksCurrent, restNodes)
-        node <- set
-      } {
+    def qqq(restNodesNew: mutable.Set[Node], set:Iterable[Node]) = {
+      set.foreach{ node =>
         if (node.data == null || node.data.version != query) {
           node.data = new Data(if (ks.contains(node)) node.inode else 0, 0, 0, query)
 
           val data = node.data
           val v = data.u
 
-          for (edge <- node.edges) {
+          var i = 0
+          while(i<node.edges.length){
+            val edge = node.edges(i)
             val o = edge.otherNode(node)
             val dataO = cacheGet(o, query)
             if (dataO != null) {
@@ -117,23 +106,33 @@ object Solution {
               data.uWithDist += dataO.uWithDist + dataO.u
               data.res += 1 * v * dataO.u + dataO.uWithDist * v + dataO.res
             }
+            i+=1
           }
 
-          for {
-            edgeI <- node.edges
-            edgeJ <- node.edges
-            if edgeI.iedge < edgeJ.iedge
-          } {
+          i = 0
+          while(i<node.edges.length) {
+            val edgeI = node.edges(i)
+            var j = i+1
             val oI = edgeI.otherNode(node)
-            val oJ = edgeJ.otherNode(node)
-
             val dataI = cacheGet(oI, query)
-            val dataJ = cacheGet(oJ, query)
+            if(dataI != null) {
+              while (j < node.edges.length) {
+                val edgeJ = node.edges(j)
 
-            if (dataI != null && dataJ != null) {
-              data.res += 2 * dataJ.u * dataI.u + dataI.uWithDist * dataJ.u + dataJ.uWithDist * dataI.u
+                val oJ = edgeJ.otherNode(node)
+
+                val dataJ = cacheGet(oJ, query)
+
+                if (dataJ != null) {
+                  data.res += 2 * dataJ.u * dataI.u + dataI.uWithDist * dataJ.u + dataJ.uWithDist * dataI.u
+                }
+
+                j += 1
+              }
             }
+            i +=1
           }
+
 
           if (node.nodeParent != null) {
             restNodesNew += node.nodeParent
@@ -146,6 +145,21 @@ object Solution {
           res = data.res
         }
       }
+    }
+
+    while (ksSorted.nonEmpty || restNodes.nonEmpty) {
+
+      var ksCurrent: Iterable[Node] = Nil
+      if (ksSorted.nonEmpty && (restNodes.isEmpty || ksSorted.head.getDistanceFromRoot() == restNodes.head.getDistanceFromRoot())) {
+        val d = ksSorted.head.getDistanceFromRoot()
+        val (ksCurrentT, ksSortedT) = ksSorted.span(node => node.getDistanceFromRoot() == d)
+        ksCurrent = ksCurrentT
+        ksSorted = ksSortedT
+      }
+
+      val restNodesNew = mutable.Set[Node]()
+      qqq(restNodesNew, ksCurrent)
+      qqq(restNodesNew, restNodes)
 
       restNodes = restNodesNew
     }
@@ -155,30 +169,34 @@ object Solution {
 
 
   def main(args: Array[String]) {
-    System.setIn(new FileInputStream(new File(s"src/main/scala/kittyscalc/in8.txt")))
+    //System.setIn(new FileInputStream(new File(s"src/main/scala/kittyscalc/in10.txt")))
 
     val sc = new java.util.Scanner(System.in)
     val (n,q) = (sc.nextInt(), sc.nextInt())
     val nodes: IndexedSeq[Node] = (0 to n).map(i => new Node(i))
-    for(iedge <- 1 to n-1) {
+    var iedge =1
+    while(iedge <= n-1){
       val edge = new Edge(iedge, nodes(sc.nextInt()), nodes(sc.nextInt()))
       edge.nodeA.addEdge(edge)
       edge.nodeB.addEdge(edge)
+      iedge+=1
     }
 
-    val t1 = System.currentTimeMillis()
+    // val t1 = System.currentTimeMillis()
     buildParentGraph(nodes)
-    println((System.currentTimeMillis()-t1)/1000.0)
-    for (i <- 1 to q) {
+    //println((System.currentTimeMillis()-t1)/1000.0)
+    var i =1
+    while(i<=q) {
       val c = sc.nextInt()
       sc.nextLine()
       val k = sc.nextLine().split(" ").map(st => st.toInt).toSet
 
       println(solve(k.map(k => nodes(k)), i))
+      i+=1
     }
 
-    val t2 = System.currentTimeMillis()
-    println((System.currentTimeMillis()-t1)/1000.0)
+    //val t2 = System.currentTimeMillis()
+    //println((System.currentTimeMillis()-t1)/1000.0)
   }
 
 }
