81e1b380f45fb3fceefd713babee8384a3b39215
David Nemeth Csoka <encse@csokavar.hu>
2017-04-15 16:31:46 +0200

    qwe

---
+++ src/main/scala/qheap1/QHeap1.scala
@@ -0,0 +1,33 @@
+package qheap1
+
+import java.io.{File, FileInputStream}
+
+import scala.collection.mutable
+
+
+object Solution {
+
+
+  def main(args: Array[String]) {
+    System.setIn(new FileInputStream(new File(s"src/main/scala/${getClass.getPackage.getName}/in1.txt")))
+    val sc = new java.util.Scanner(System.in)
+
+    val q = sc.nextInt()
+    val heap = mutable.SortedSet[Int]()
+    for (i <- 1 to q) {
+      sc.nextInt() match {
+        case 1 =>
+          val v = sc.nextInt()
+          heap.add(v)
+        case 2 =>
+          val v = sc.nextInt()
+          heap.remove(v)
+        case 3 =>
+          println(heap.head)
+      }
+    }
+  }
+
+}
+
+
